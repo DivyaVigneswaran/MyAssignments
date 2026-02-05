a4192c21623eea4178ef873dd644be3ec4588561
DivyaVigneswaran <divyaseelan1994@gmail.com>
2026-02-05 14:51:52 +0530

    Add files via upload
    Day1 - Datatypes

---
+++ day1/Sample.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Introduction to Java");
+		String username = "Divya";
+		char lastname = 'V';
+		int age = 20;
+		boolean major = true;
+		float height = 175.8f;
+		long contactNumber = 987654321098l;
+		System.out.println("Username - "+username);
+		System.out.println("Lastname - " +lastname);
+		System.out.println("Age - " +age);
+		System.out.println("Major - " +major);
+		System.out.println("Height - " +height);
+		System.out.println("ContactNumber - " +contactNumber);
+		
+		
+	}
+
+}
